185452dfb47ff9f7d4da04d572f4a7aead0f66d8
Ali Ashkani Nia <ashkaninia@gmail.com>
2024-01-06 21:19:26 +0330

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(){
+    std::cout << "hii pool \n";
+    return 0;
+}
---
+++ tests/utility_test.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+
+// Define a test case
+TEST(HelloWorldTest, BasicAssertion) {
+    // Replace this assertion with the actual condition you want to test
+    ASSERT_TRUE(true);
+
+    // You can use other assertions like ASSERT_EQ, ASSERT_NE, etc.
+    // For example:
+    // ASSERT_EQ(2 + 2, 4);
+}
