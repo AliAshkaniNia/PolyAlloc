cd3a1f10911b0bc337c95b6713b686432c59d2a6
Ali Ashkani Nia <ashkaninia@gmail.com>
2024-01-07 00:52:51 +0330

    Added printArrayContents utility function * Added static analyser

---
+++ include/utility.h
@@ -0,0 +1,61 @@
+#ifndef __UTILITY_H__
+#define __UTILITY_H__
+
+#include <iostream>
+#include <string_view>
+#include <array>
+#include <format>
+#include <cstdint>
+
+namespace util
+{
+
+    template <size_t X>
+    void printArrayContents(std::string_view title,  std::array<std::byte, X>& data) {
+        constexpr size_t numbers_per_line = 16;
+
+
+        std::cout << std::format("==============={:^10}==============\n", title);
+        std::cout << std::format("Buffer Address Start: 0x{:x}\n", reinterpret_cast<uintptr_t>(data.data()));
+
+
+        for (size_t i = 0; i < X; ++i) {
+            // Start a new line every 'numbers_per_line' elements
+            if (i % numbers_per_line == 0) {
+                if (i != 0) {
+                    std::cout << '\n';
+                }
+                std::cout << std::format("(dec) {:02x}: ", i);
+            }
+
+            // Print the current element in decimal
+            std::cout << std::format(" {:4}", static_cast<unsigned>(data[i]));
+            
+            // At the end of a line or the end of data, print hex and ascii values
+            if ((i + 1) % numbers_per_line == 0 || i + 1 == X) {
+                // Print hex values
+                std::cout << '\n' << std::format("(hex) {:02x}: ", (i / numbers_per_line) * numbers_per_line);
+                for (size_t j = i - (i % numbers_per_line); j <= i; ++j) {
+                    std::cout << std::format("   {:-02x}", static_cast<unsigned>(data[j]));
+                }
+                
+                // Print ASCII characters or octal values for non-printable characters
+                std::cout << '\n' << std::format("(asc) {:02x}: ", (i / numbers_per_line) * numbers_per_line);
+                for (size_t j = i - (i % numbers_per_line); j <= i; ++j) {
+                    char ch = static_cast<char>(data[j]);
+                    if (isprint(static_cast<unsigned char>(ch))) {
+                        std::cout << std::format("   {} ", static_cast<char>(ch));
+                    } else {
+                        std::cout << std::format(" \\{:03o}", ch);
+                        
+                    }
+                }
+                std::cout << '\n';
+            }
+        }
+    }
+    
+
+} // namespace util
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
+#include "utility.h"
+#include <array>
 #include <iostream>
+#include <vector>
 
-int main(){
+auto main()->int{
     std::cout << "hii pool \n";
+    constexpr int buffer_size = 32;
+    std::array<std::byte, buffer_size> arr{};
+    util::printArrayContents("Initial state",arr);
+
     return 0;
 }
